8a870a57b7599ed3f60ffe89af048c0f0ae548f8
franciapparelli <>
2023-11-06 09:36:48 -0300

    Boats Tracker Fixed

--- app/main.py
+++ app/main.py
@@ -13,20 +13,19 @@ LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 
 FPS = 60
 
-crew_img = pygame.image.load("images/crew2.png")
+crew_img = pygame.image.load("crew2.png")
 rect = crew_img.get_rect()
 rect.center = WIDTH // 2, HEIGHT // 2
 
-START_BUTTON = pygame.image.load("images/start.png")
+START_BUTTON = pygame.image.load("start.png")
 rect_start = START_BUTTON.get_rect()
 rect_start.center = WIDTH // 2, HEIGHT // 1.3
 
-BULLET = pygame.image.load("images/bullet.png")
+BULLET = pygame.image.load("bullet.png")
 
 # we use the sizes to draw as well as to do our "steps" in the loops.
 GRID_NODE_WIDTH = WIDTH // 25
 GRID_NODE_HEIGHT = WIDTH // 25
-
 
 def draw_window():
     SCREEN.fill(LIGHT_BLUE)
@@ -106,76 +105,34 @@ def visualize_grid_computer(matrix):
             x += GRID_NODE_WIDTH  # for every item/number in that row we move one "step" to the right
         y += GRID_NODE_HEIGHT  # for every new row we move one "step" downwards
 
-"""
+
 def validate_ship_placement(matrix):
-    # Define the ship sizes and their counts
-    ship_sizes = {'Aircraft Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine': 3, 'Destroyer': 2}
-    ship_counts = {ship: 0 for ship in ship_sizes}
-
-    # Iterate through the matrix to validate ship placement
-    for row in range(9):
-        for col in range(9):
-            cell = matrix[row][col]
-
-            # If the cell contains a ship, check its placement
-            if cell == "H":
-                # Check horizontally for consecutive ships
-                if col + 1 < 9 and matrix[row][col + 1] == "H":
-                    return False  # Two ships are adjacent horizontally
-                # Check vertically for consecutive ships
-                if row + 1 < 9 and matrix[row + 1][col] == "H":
-                    return False  # Two ships are adjacent vertically
-
-                # Determine the ship's size
-                ship_size = 1
-                if col + 1 < 9 and matrix[row][col + 1] == "H":
-                    # If the ship continues horizontally, calculate its size
-                    for i in range(col + 1, 9):
-                        if matrix[row][i] == "H":
-                            ship_size += 1
-                        else:
-                            break
-
-                elif row + 1 < 9 and matrix[row + 1][col] == "H":
-                    # If the ship continues vertically, calculate its size
-                    for i in range(row + 1, 9):
-                        if matrix[i][col] == "H":
-                            ship_size += 1
-                        else:
-                            break
-
-                # Check if the ship size matches a valid ship size
-                if ship_size not in ship_sizes.values():
-                    return False  # Invalid ship size
-
-                # Increment the count of the corresponding ship
-                for ship, size in ship_sizes.items():
-                    if size == ship_size:
-                        ship_counts[ship] += 1
-
-    # Check if the correct number of each ship is placed
-    for ship, count in ship_counts.items():
-        if count != 1:  # In a standard game, there should be one of each type of ship
-            return False
-
-    # All checks passed, ship placement is valid
+    counter = 0
+    for row in matrix:
+        for item in row:
+            if item == "B":
+                counter += 1
+    if counter != 12:
+        print(counter)
+        return False
     return True
-"""
-
 
 def computer_attacking(matrix, computer_counter):
     row = random.randint(0, 8)
     column = random.randint(0, 8)
 
-    while matrix[row][column] != "F" and matrix[row][column] != "O":
+    while matrix[row][column] == "F" or matrix[row][column] == "O":
         row = random.randint(0, 8)
         column = random.randint(0, 8)
-        if matrix[row][column] == "X":
-            matrix[row][column] = "F"
-            computer_counter += 1
-        else:
-            matrix[row][column] = "O"
-    return matrix, computer_counter
+
+    if matrix[row][column] == "B":
+        matrix[row][column] = "F"
+        computer_counter += 1  # Increment the computer_counter
+    else:
+        matrix[row][column] = "O"
+
+    return matrix, computer_counter  # Return the updated computer_counter
+
 
 Message = namedtuple("Message", ["text", "position", "duration", "frames"])
 
@@ -201,19 +158,19 @@ def main():
     clock = pygame.time.Clock()
 
     # Crew Boats Load
-    crew2_img = pygame.image.load("images/crew2.png")
+    crew2_img = pygame.image.load("crew2.png")
     rect2 = crew2_img.get_rect()
     rect2.center = WIDTH // 9, HEIGHT // 1.2
 
-    crew3_img = pygame.image.load("images/crew3.png")
+    crew3_img = pygame.image.load("crew3.png")
     rect3 = crew3_img.get_rect()
-    rect3.center = WIDTH // 3, HEIGHT // 1.2  
+    rect3.center = WIDTH // 3, HEIGHT // 1.2
 
-    crew3D_img = pygame.image.load("images/crew3.png") # Second Boat of size 3
+    crew3D_img = pygame.image.load("crew3.png") # Second Boat of size 3
     rect3D = crew3D_img.get_rect()
-    rect3D.center = WIDTH // 9, HEIGHT // 1.1 
+    rect3D.center = WIDTH // 9, HEIGHT // 1.1
 
-    crew4_img = pygame.image.load("images/crew4.png")
+    crew4_img = pygame.image.load("crew4.png")
     rect4 = crew4_img.get_rect()
     rect4.center = WIDTH // 3, HEIGHT // 1.1
 
@@ -254,11 +211,11 @@ def main():
                         moving = True
                         moving4 = True
                     if rect_start.collidepoint(event.pos):
-                        # if validate_ship_placement(player_matrix):
-                        #     ships_placing_phase = False
-                        ships_placing_phase = False
-                        attacking_phase = True
-                        START_BUTTON.fill((255, 255, 255, 0))
+                        print(player_matrix)
+                        if validate_ship_placement(player_matrix):
+                            ships_placing_phase = False
+                            attacking_phase = True
+                            START_BUTTON.fill((255, 255, 255, 0))
                 if event.button == 3:
                     if rect2.collidepoint(event.pos):
                         crew2_img = pygame.transform.rotate(crew2_img, 90)
@@ -296,6 +253,11 @@ def main():
                 first_node_y = (first_pos_y - (HEIGHT // 8)) // GRID_NODE_HEIGHT
                 second_node_x = (second_pos_x - (WIDTH // 16)) // GRID_NODE_WIDTH
                 second_node_y = (second_pos_y - (HEIGHT // 8)) // GRID_NODE_HEIGHT
+                for row in range(9):
+                    for item in range(9):
+                          if player_matrix[row][item] == "H":
+                            player_matrix[row][item] = "B"
+
             elif event.type == pygame.MOUSEMOTION and moving and ships_placing_phase:
                 if moving2:
                     rect2.move_ip(event.rel)
@@ -330,28 +292,28 @@ def main():
                                 and first_node_x == second_node_x
                                 or first_node_y == second_node_y
                             ):
-                                if moving2:        
+                                if moving2:
                                     if second_node_x - first_node_x == 1 or second_node_y - first_node_y == 1:
                                         player_matrix[row][item] = "H"
                                     else:
                                         player_matrix[row][item] = "F"
-                                elif moving3 or moving3D:        
+                                elif moving3 or moving3D:
                                     if second_node_x - first_node_x == 2:
                                         player_matrix[row][item] = "H"
-                                        if item != first_node_x: 
+                                        if item != first_node_x:
                                             player_matrix[row][item - 1] = "H"
                                     elif second_node_y - first_node_y == 2:
                                         player_matrix[row][item] = "H"
-                                        if row != first_node_y:    
+                                        if row != first_node_y:
                                             player_matrix[row - 1][item] = "H"
                                     else:
                                         if second_node_x - first_node_x == 3:
-                                            player_matrix[row][item] = "F" 
+                                            player_matrix[row][item] = "F"
                                             player_matrix[row][item - 1] = "F"
                                         if second_node_y - first_node_y == 3:
-                                            player_matrix[row][item] = "F"    
+                                            player_matrix[row][item] = "F"
                                             player_matrix[row - 1][item] = "F"
-                                elif moving4:        
+                                elif moving4:
                                     if second_node_x - first_node_x == 3 and second_node_x < 9:
                                         player_matrix[row][item] = "H"
                                         player_matrix[row][second_node_x - 1] = "H"
@@ -363,7 +325,9 @@ def main():
                                     else:
                                         player_matrix[row][item] = "F"
                         else:
-                            player_matrix[row][item] = "-"
+                            if player_matrix[row][item] != "B":
+                                player_matrix[row][item] = "-"
+
             elif event.type == pygame.MOUSEBUTTONDOWN and attacking_phase:
                 if player_attacking_phase:
                     message = create_message("Player 1's turn", (10, 10), 120)  # Display for 2 seconds (assuming 60 FPS)
@@ -392,6 +356,7 @@ def main():
                             player_attacking_phase = False
                             if player_counter < 12:
                                 player_matrix, computer_counter = computer_attacking(player_matrix, computer_counter)
+                                print(computer_counter)
                                 player_attacking_phase = True
 
         draw_window()
